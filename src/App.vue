781b667d60d026634888bca491fd4a08386e192d
zhangss <shan5858kda@163.com>
2024-12-04 20:29:37 +0800

    feat: 初始化框架

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+<my-comp 
+:abc="fatherText" 
+:changeClick="changeClick">
+</my-comp>
+</template>
+
+<script>
+import { ref } from 'vue'
+import myComp from './views/myComp.vue'
+export default {
+  components: { myComp },
+
+  setup() {
+    const fatherText = ref("父组件传值")
+    function changeClick(val){
+      fatherText.value = val;
+    }
+    return {
+      fatherText,
+      changeClick
+    }
+  }
+}
+</script>
---
+++ src/views/myComp.vue
@@ -0,0 +1,24 @@
+<template>
+  <button @click="changeClick('子组件修改')">change</button>
+  <div>{{abc}}</div>
+</template>
+
+<script>
+export default {
+
+  props: {
+    abc: {
+      type: String,
+      defult: null
+    },
+    changeClick: {
+      type:Function
+    }
+  },
+
+  setup() {
+    return {
+    }
+  }
+}
+</script>
